3200640f4e5ce822cb9cc768c983de2e4fa86d89
Aref Yazdkhasti <aref.yazdkhasti@gmail.com>
2021-07-02 11:39:57 +0430

    registerging added

--- app/src/main/java/com/example/onesignaldemo/MainActivity.kt
+++ app/src/main/java/com/example/onesignaldemo/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.onesignaldemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.onesignal.OneSignal
 
 class MainActivity : AppCompatActivity() {
+
+    //app id from onesignal
+    companion object {
+        const val ONESIGNAL_APP_ID = "0edc605b-d5ce-4269-a4eb-654d136fd9a0"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //registering to onesignal
+
+        // Logging set to help debug issues, remove before releasing your app.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
+
     }
 }
